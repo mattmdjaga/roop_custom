9d12255525514cdba6a8a36b37ad3501c5f8304a
Maks <maks@pons.ai>
2025-08-28 16:02:41 +0000

    Adjust utils to import codeformer differently

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ from basicsr.utils.download_util import load_file_from_url
 from facelib.utils.face_restoration_helper import FaceRestoreHelper
 from facelib.utils.misc import is_gray
 from basicsr.archs.rrdbnet_arch import RRDBNet
-from basicsr.utils.realesrgan_utils import RealESRGANer
+from CodeFormer.basicsr.utils.realesrgan_utils import RealESRGANer
 
 from basicsr.utils.registry import ARCH_REGISTRY
 
